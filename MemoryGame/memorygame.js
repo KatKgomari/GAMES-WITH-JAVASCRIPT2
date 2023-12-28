f65188de13c1d9e161bdfd32b5c466942131e623
Katlego Kgomari <kgomari88@gmail.com>
2023-12-28 14:26:22 +0200

    Put everything in folders

---
+++ MemoryGame/memorygame.js
@@ -0,0 +1,11 @@
+// we will add most of our elements using javascript
+// we add some elements to create our grid 
+
+
+//Creating an array of objects
+const cardArray = [
+    {
+        name: "fries",
+        img: 
+    }
+];
